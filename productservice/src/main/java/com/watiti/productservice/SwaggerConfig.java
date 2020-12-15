22bb79638dd112c8d0b4a9d56e47eea268aeae06
Brian Nkuru <nkurunungib@gmail.com>
2020-12-15 10:51:57 -0600

    The pakage scanne path

--- productservice/src/main/java/com/watiti/productservice/SwaggerConfig.java
+++ productservice/src/main/java/com/watiti/productservice/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("edu.miu.productservice"))
+                .apis(RequestHandlerSelectors.basePackage("com.watiti.productservice"))
                 .build();
 
     }
